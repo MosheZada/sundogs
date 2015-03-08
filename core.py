49cbc138bc7febda70b254bcd35118a7b91c6d4c
Moshe Zada <moshe@forter.com>
2015-03-08 22:29:34 +0200

    Add core queries files

---
+++ core.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import queries
+from pykwalify.core import Core
+
+from math import sqrt
+from elasticsearch import Elasticsearch
+
+
+def connect_to_es(ip):
+    return Elasticsearch(ip)
+
+
+def get_stats(settings, conn):
+    conn.ping()
+
+    query = queries.get_data_histogram_query(settings['time_aggregator'],
+                                             settings['time_window'],
+                                             settings["must"],
+                                             settings["must_not"],
+                                             settings["should"])
+    res = conn.search(index="today", body=query)
+    return res['aggregations']['events_by_date']['buckets']
+
+
+def check_anomaly(sample, entries, std_multiply, chart_value='max'):
+    n = 0
+    mean = 0
+    m2 = 0
+    max_value = None
+    for entry in entries:
+        value = entry['stats_0'][chart_value]
+        n += 1
+        delta = value - mean
+        mean += delta / n
+        m2 += delta * (value - mean)
+        max_value = max(max_value, value)
+    variance = m2 / (n - 1)
+    std = sqrt(variance)
+    logging.info("sample: %s std : %s, mean : %s, max: %s, lol: %s" %
+                 (sample['stats_0'][chart_value], std, mean, max_value, mean + 3 * std))
+    if sample['stats_0'][chart_value] > mean + std_multiply * std:
+        return sample
+    else:
+        return False
+
+
+def load_settings():
+    logging.info("Validating settings.yaml")
+    c = Core(source_file="settings.yaml", schema_files=["schema.yaml"])
+    settings = c.validate(raise_exception=True)
+
+    for probe_settings in settings["probes"]:
+        if "must" not in probe_settings:
+            probe_settings.update({"must": []})
+        if "must_not" not in probe_settings:
+            probe_settings.update({"must_not": []})
+        if "should" not in probe_settings:
+            probe_settings.update({"should": []})
+    return settings
+
+
+def set_logging():
+    logging.getLogger("paramiko").setLevel(logging.WARNING)
+    logging.getLogger("elasticsearch").setLevel(logging.WARNING)
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+    logging.basicConfig(level=logging.DEBUG, datefmt='%H:%M:%S',
+                        format='[%(asctime)s] {%(name)10s:%(lineno)3d} %(levelname)8s - %(message)s')
+
+
+def main():
+    set_logging()
+    settings = load_settings()
+    conn = connect_to_es(settings['settings']['es_host'])
+
+    for probe_settings in settings['probes']:
+        stats = get_stats(probe_settings, conn)
+        for test in xrange(1, probe_settings["times_to_run"] + 1):
+            sample = stats[-test]
+            data = stats[:-test]
+            std_multiply = probe_settings["std_multiply"]
+            print check_anomaly(sample, data, std_multiply)
---
+++ queries.py
@@ -0,0 +1,50 @@
+from elasticsearch_dsl import Search, Q
+
+
+def get_data_histogram_query(interval, time_window, must_data, must_not_data, should_data, value_field="metric",
+                             by=None):
+    must = [Q("range", **{"@timestamp": {"from": "now-" + time_window, "to": "now"}})]
+    must_not = []
+    should = []
+
+    for element in must_data:
+        must.append(Q("term", **{element["key"]: element["value"]}))
+
+    for element in must_not_data:
+        must_not.append(Q("term", **{element["key"]: element["value"]}))
+
+    for element in should_data:
+        should.append(Q("term", **{element["key"]: element["value"]}))
+
+    s = Search().query(Q('bool',
+                         must=must,
+                         must_not=must_not))
+
+    query = s.to_dict()
+    date_histogram = {
+        "events_by_date": {
+            "date_histogram": {
+                "field": "@timestamp",
+                "interval": interval
+            },
+            "aggs": {
+                "stats_0": {
+                    "stats": {
+                        "field": value_field
+                    }
+                }
+            }
+        }
+    }
+    if by is not None:
+        query.update({"aggs": {
+            "events_by": {
+                "terms": {
+                    "field": by
+                },
+                "aggs": date_histogram,
+            }
+        }})
+    else:
+        query.update({"aggs": date_histogram})
+    return query
