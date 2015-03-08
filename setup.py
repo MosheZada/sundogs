73b92c509207bf0eda08fa97e2a298f3761483ab
Moshe Zada <moshe@forter.com>
2015-03-08 22:42:38 +0200

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='sundogs',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/MosheZada/sundogs/',
+    license='',
+    author='Moshe Zada',
+    author_email='moshe@forter.com',
+    description='', requires=['pykwalify', 'elasticsearch']
+)
